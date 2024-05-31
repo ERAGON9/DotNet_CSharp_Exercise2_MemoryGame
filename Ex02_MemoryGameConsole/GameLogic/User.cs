bfe046ea6233769d8aa88c51de9e1eb8a9bb96bb
ERAGON9 <liorbarak99@gmail.com>
2024-05-31 22:28:36 +0300

    Starting make the game and initial the project.

---
+++ Ex02_MemoryGameConsole/GameLogic/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02_MemoryGameConsole.GameLogic
+{
+    internal class User
+    {
+        private readonly string m_Name;
+        public string Name {
+            get
+            {
+                return m_Name;
+            }
+        }
+
+        public User(string i_UserName)
+        {
+            m_Name = i_UserName;
+        }
+    }
+}
---
+++ Ex02_MemoryGameConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using Ex02_MemoryGameConsole.GameLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02_MemoryGameConsole
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter your name:");
+            string playerName = Console.ReadLine();
+            User player1 = new User(playerName);
+            Console.WriteLine("The game is against player or computer? " +
+                "(enter: player/computer)");
+            string opponent = Console.ReadLine();
+            if (opponent == "player")
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
