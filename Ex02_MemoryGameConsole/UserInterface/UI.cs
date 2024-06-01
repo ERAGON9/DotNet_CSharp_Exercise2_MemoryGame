88a04579549103b071e6b90eeab7f65b7e8241b6
Noa Halali <noaha@mta.ac.il>
2024-06-01 16:28:29 +0300

    /

--- Ex02_MemoryGameConsole/UserInterface/UI.cs
+++ Ex02_MemoryGameConsole/UserInterface/UI.cs
@@ -18,11 +18,12 @@ namespace Ex02_MemoryGameConsole.UserInterface
             int boardWidth, boardHeight;
 
             receiveGamePropertiesFromUser(out Player1Name, out Player2Name, out boardWidth, out boardHeight);
+
             game = new GameData(Player1Name, Player2Name, boardHeight, boardWidth);
 
 
 
-            while (!isGameOver(gameData))
+            while (!isGameOver(game))
             {
                 //print board
 
@@ -147,12 +148,30 @@ namespace Ex02_MemoryGameConsole.UserInterface
                 measure = tmpMeasure;
             }
 
-            return measure.Value; //Always get value 
+            return measure.Value; //Always get value because always first iteration happens
         }
         //private bool checkIfValidMeasureInput(string i_MeasureStr)
         //{
         //    bool isInteger, isPositive;
-            
+
         //}
+
+
+        //change accordings to the board in the instructions
+        public void PrintBoard(Card[,] i_CardsMatrix)
+        {
+            int rows = i_CardsMatrix.GetLength(0);
+            int cols = i_CardsMatrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(i_CardsMatrix[i, j].Content + " ");
+                }
+                Console.WriteLine();
+            }
+        }
     }
+
 }
