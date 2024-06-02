4ec02ac6466d4cf1f75d43cc7ae28aa27bc8fa53
ERAGON9 <liorbarak99@gmail.com>
2024-06-02 16:36:27 +0300

    After UI changes.

--- Ex02_MemoryGameConsole/UserInterface/UI.cs
+++ Ex02_MemoryGameConsole/UserInterface/UI.cs
@@ -11,104 +11,163 @@ namespace Ex02_MemoryGameConsole.UserInterface
     internal class UI
     {
         private GameData m_GameEngine;
+        private bool m_StillPlaying = true;
+        //private bool m_PlayerQuit = false;
 
         public void runGame()
         {
 
-            try
+
+            setGameProperties();
+            setBoardGame();
+
+            while (m_StillPlaying)
             {
-                SetGameProperties();
-                SetBoardGame();
+                Screen.Clear();
 
-                while (!isGameOver())
+                printBoard();
+
+                /*choose square*/
+                // get 1 square   (if press Q - break)
+                pickSquare();
+                if (!m_StillPlaying)
                 {
-                    Screen.Clear();
-
-                    printBoard(); // update if flip card or not.
-
-                    /*choose square*/
-                    // get 1 square   (if press Q - break)
-                    pickCard();
-
-                    Screen.Clear();
-                    printBoard();
-                    
-                    /*choose square again*/   //(if press Q)
-                                              //need to return the square content
-                    pickCard();
-
-                    //logic - check if the 2 cards the same
-                    //if the same:
-                    //   givePoint()    
-
-                    // else: (if not the same:)
-                    //      UI-show 2 sec, logic - flip back.
-                    //      switch current player
-
+                    break;
                 }
 
-                // print the ponits of each player- with player win.
-                // if want another game or not.
+                Screen.Clear();
+                printBoard();
+
+                /*choose square again*/   //(if press Q)
+                                          //need to return the square content
+                pickSquare();
+                if (!m_StillPlaying)
+                {
+                    break;
+                }
+
+                //logic - check if the 2 cards the same
+                //if the same:
+                //   givePoint()    
+
+                // else: (if not the same:)
+                //      UI-show 2 sec, logic - flip back.
+                //      switch current player
+
+                m_StillPlaying = !isGameOver();
             }
-            catch (Exception ex)
+
+            if (isGameOver()) 
             {
-                Console.WriteLine(ex.Message);
+                printStatistics();
             }
+            else
+            {
+                Console.WriteLine("Player choose to Quit during the game.");
+                Console.WriteLine("Game closing, bye bye.");
+            }
+
+
+            // print the ponits of each player- with player win.
+            // if want another game or not.
         }
 
-        private void pickCard()
+        private void pickSquare()
         {
             bool isValidSquare = false;
-            string square;
+            string square, errorMessage;
+
             while (!isValidSquare) //logically
             {
-                square = getSquare(); //UI (check in while)
-                if (square == "Q")
-                    break;
-                // logic - check  if!(in board and not taken)
-                isValidSquare = m_GameEngine.IsValidSquare(); // Logic
-                if (!isValidSquare)
+                square = getSquare();
+                if (!m_StillPlaying)
                 {
-                    // UI - print error message
+                    return;
                 }
 
+                // logic - check  if not in board and not taken.
+                isValidSquare = m_GameEngine.IsValidSquare(square, out errorMessage); // Logic
+                if (!isValidSquare)
+                {
+                    Console.WriteLine(errorMessage);
+                }
             }
+
             m_GameEngine.FlipCard(square);
-            m_GameEngine.Turn.flipCard1();
-            m_GameEngine.Turn.flipCard2();
+            m_GameEngine.flipCard1();
+            m_GameEngine.flipCard2();
         }
-        private void SetGameProperties()
+
+        private string getSquare()
+        {
+            string playerInput = string.Empty; //Always get value because, always first
+                                               //iteration of while loop happens.
+            bool isValidSquare = false;
+
+            while (!isValidSquare)
+            {
+                Console.WriteLine("Please enter square to uncover " +
+                    "letter for column and number for row (like: B2) or Q for Quit");
+                playerInput = Console.ReadLine();
+                if (isUserQuit(playerInput))
+                {
+                    m_StillPlaying = false;
+                    break;
+                }
+
+                isValidSquare = squareValidation(playerInput);
+            }
+
+            return playerInput;
+        }
+
+        private bool isUserQuit(string i_UserInput)
+        {
+            bool isUserQuit = i_UserInput == "Q";
+            
+            return isUserQuit;
+        }
+
+        private bool squareValidation(string i_Square)
+        {
+            bool isValid = true;
+
+            if (i_Square.Length != 2 || !char.IsLetter(i_Square[0])
+                || !char.IsDigit(i_Square[1]))
+            {
+                Console.WriteLine("Input is not 1 letter and 1 digit. (at the form of: B2)");
+                isValid = false;
+            }
+
+            return isValid;
+        }
+        private void setGameProperties()
         {
             string player1Name, player2Name;
             bool gameAgainstComputer;
 
             player1Name = receivePlayerName();
             player2Name = chooseAndReceiveSecoundPlayer(out gameAgainstComputer);
-            try
-            {
-                m_GameEngine = new GameData(player1Name, player2Name, gameAgainstComputer);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                SetGameProperties();
-            }
+
+            m_GameEngine = new GameData(player1Name, player2Name, gameAgainstComputer);
         }
 
-        private void SetBoardGame()
+        private void setBoardGame()
         {
             int boardWidth, boardHeight;
+            bool initialSucceeded = false;
+            string errorMessage;
 
-            boardWidth = receiveBoardWidth();
-            boardHeight = receiveBoardHeight();
-            try
+            while (!initialSucceeded)
             {
-                m_GameEngine.InitialCardsMatrix(boardHeight, boardWidth);
-            }
-            catch (Exception ex) 
-            {
-                Console.WriteLine(ex.Message);
-                SetBoardGame();
+                boardWidth = receiveBoardWidth();
+                boardHeight = receiveBoardHeight();
+
+                initialSucceeded = m_GameEngine.InitialCardsMatrix(boardHeight, boardWidth, out errorMessage);
+                if (!initialSucceeded)
+                {
+                    Console.WriteLine(errorMessage + " try again.");
+                }
             }
         }
 
@@ -260,10 +319,20 @@ namespace Ex02_MemoryGameConsole.UserInterface
 
             for (int i = 0; i < i_Rows; i++)
             {
-                stringToPrint.Append(i + 1 + " ");
+                stringToPrint.Append((i+1) + " ");
                 for (int j = 0; j < i_Cols; j++)
                 {
-                    stringToPrint.Append("|   ");
+                    stringToPrint.Append("| ");
+                    if (m_GameEngine.CardsMatrix[i,j].IsFlipped == false)
+                    {
+                        stringToPrint.Append(' ');
+                    }
+                    else
+                    {
+                        stringToPrint.Append(m_GameEngine.CardsMatrix[i, j].Content);
+                    }
+
+                    stringToPrint.Append(' ');
                 }
 
                 stringToPrint.Append('|');
